abb6dc8000c10c3c9e650bdba358551527a29bf3
Ricardo Paes <ricardo@likesistemas.com.br>
2024-12-06 10:51:51 -0300

    ✨ [GEST-2428] `LoggerTest` Implementation that already existed in psr/log v1.0

--- src/TestLogger.php
+++ src/TestLogger.php
@@ -58,7 +58,7 @@ class TestLogger extends AbstractLogger {
 
 	public $recordsByLevel = [];
 
-	public function log($level, $message, array $context = []):void {
+	public function log($level, $message, array $context = []): void {
 		$record = [
 			'level' => $level,
 			'message' => $message,
